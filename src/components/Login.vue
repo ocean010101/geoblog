991315985979b6d022344386c46c437a05a8f771
ocean010101 <yangyang01.xu@outlook.com>
2019-10-01 18:40:14 +0800

    Update Login component
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,12 +9,36 @@
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 export default {
   methods: {
+    ...mapActions([
+      'login',
+    ]),
+
     openGoogleSignin () {
-      //TODO
-    }
+      const url = 'http://localhost:3000/auth/google'
+      const name = 'google_login'
+      const specs = 'width=500,height=500'
+      window.open(url, name, specs)
+    },
+
+    handleMessage ({data, origin}) {
+      if (origin !== 'http://localhost:3000') {
+        return
+      }
+
+      if (data === 'success') {
+        this.login()
+      }
+    },
+  },
+  mounted () {
+    window.addEventListener('message', this.handleMessage)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('message', this.handleMessage)
   },
 }
 </script>
