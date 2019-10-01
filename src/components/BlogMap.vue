f20ec217701fd995d7c98bc7f1e5c9b5ace783eb
ocean010101 <yangyang01.xu@outlook.com>
2019-10-01 21:22:19 +0800

    Add  BlogMap component
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

---
+++ src/components/BlogMap.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="blog-map">
+    <googlemaps-map
+      ref="map"
+      :center="center"
+      :zoom="zoom"
+      :options="mapOptions"
+      @update:center="setCenter"
+      @update:zoom="setZoom">
+    </googlemaps-map>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            center: {
+                lat: 48.8538302,
+                lng: 2.2982161
+            },
+            zoom: 15
+        }
+    },
+    computed: {
+
+        mapOptions() {
+            return {
+                fullscreenControl: false,
+            }
+        },
+    },
+
+    methods: {
+        setCenter(value) {
+            this.center = value;
+        },
+        setZoom(value) {
+            this.zoom = value;
+
+        }
+    },
+}
+</script>
--- src/components/GeoBlog.vue
+++ src/components/GeoBlog.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="geo-blogs">
     <AppMenu />
+    <div class="panes">
+      <BlogMap />
+    </div>
   </div>
 </template>
 <script>
 import AppMenu from './AppMenu'
+import BlogMap from './BlogMap'
 export default {
   components: {
-    AppMenu
+    AppMenu,
+    BlogMap
   }
 }
 </script>
