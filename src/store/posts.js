75c3f6475d3ed8dd7f1491524733cfb51b98ef0a
ocean010101 <yangyang01.xu@outlook.com>
2019-10-02 10:41:32 +0800

    Update CreatePost component
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- src/store/posts.js
+++ src/store/posts.js
@@ -1,3 +1,4 @@
+import { $fetch } from '../plugins/fetch'
 
 export default {
     namespaced: true,
@@ -59,6 +60,29 @@ export default {
                 position: null,
                 placeId: null,
             })
+        },
+
+        async createPost({ commit, dispatch }, draft) {
+            const data = {
+                ...draft,
+                // 需要获取表单对象
+                position: draft.position.toJSON(),
+            }
+
+            // 发送请求
+            const result = await $fetch('posts/new', {
+                method: 'POST',
+                body: JSON.stringify(data),
+            })
+            dispatch('clearDraft')
+
+            // 更新博客列表
+            commit('addPost', result)
+            dispatch('selectPost', result._id)
+        },
+        //新建的博客会被自动选中
+        async selectPost({ commit, getters }, id) {
+            commit('selectedPostId', id)
         },
         //点击地图时调用
         setDraftLocation({ dispatch, getters }, { position, placeId }) {
